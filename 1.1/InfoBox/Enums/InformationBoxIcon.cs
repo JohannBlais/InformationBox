ba0ef492acf829de9860ef656fa4c1ce6b4f0981
SaumonAgile_cp <SND\SaumonAgile_cp>
2007-07-12 08:36:22 +0000

    Added new icons

--- 1.1/InfoBox/Enums/InformationBoxIcon.cs
+++ 1.1/InfoBox/Enums/InformationBoxIcon.cs
@@ -5,45 +5,81 @@ namespace InfoBox
     /// </summary>
     public enum InformationBoxIcon
     {
-        /// <summary>
-        /// Asterisk
-        /// </summary>
-        Asterisk,
-        /// <summary>
-        /// Error
-        /// </summary>
-        Error,
-        /// <summary>
-        /// Exclamation
-        /// </summary>
-        Exclamation,
-        /// <summary>
-        /// Hand
-        /// </summary>
-        Hand,
-        /// <summary>
-        /// Information
-        /// </summary>
-        Information,
-        /// <summary>
-        /// None
-        /// </summary>
-        None,
-        /// <summary>
-        /// Question
-        /// </summary>
-        Question,
-        /// <summary>
-        /// Stop
-        /// </summary>
-        Stop,
-        /// <summary>
-        /// Warning
-        /// </summary>
-        Warning,
-        /// <summary>
-        /// Success
-        /// </summary>
-        Success,
+		/// <summary>
+		/// Asterisk
+		/// </summary>
+		Asterisk,
+		/// <summary>
+		/// Error
+		/// </summary>
+		Error,
+		/// <summary>
+		/// Exclamation
+		/// </summary>
+		Exclamation,
+		/// <summary>
+		/// Hand
+		/// </summary>
+		Hand,
+		/// <summary>
+		/// Information
+		/// </summary>
+		Information,
+		/// <summary>
+		/// None
+		/// </summary>
+		None,
+		/// <summary>
+		/// Question
+		/// </summary>
+		Question,
+		/// <summary>
+		/// Stop
+		/// </summary>
+		Stop,
+		/// <summary>
+		/// Warning
+		/// </summary>
+		Warning,
+		/// <summary>
+		/// Success
+		/// </summary>
+		Success,
+		/// <summary>
+		/// Fax
+		/// </summary>
+		Fax,
+		/// <summary>
+		/// Gamepad
+		/// </summary>
+		Gamepad,
+		/// <summary>
+		/// Joystick
+		/// </summary>
+		Joystick,
+		/// <summary>
+		/// Keys
+		/// </summary>
+		Keys,
+		/// <summary>
+		/// Locker
+		/// </summary>
+		Locker,
+		/// <summary>
+		/// Phone
+		/// </summary>
+		Phone,
+		/// <summary>
+		/// Printer
+		/// </summary>
+		Printer,
+		/// <summary>
+		/// Scanner
+		/// </summary>
+		Scanner,
+		/// <summary>
+		/// Speakers
+		/// </summary>
+		Speakers,
     }
 }
