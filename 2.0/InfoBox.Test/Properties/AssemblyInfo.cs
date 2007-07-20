90b5457fa25c8fba5468c8d1237c01222d772662
SaumonAgile_cp <SND\SaumonAgile_cp>
2007-07-20 13:47:23 +0000

    Updated version number

--- 2.0/InfoBox.Test/Properties/AssemblyInfo.cs
+++ 2.0/InfoBox.Test/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.4.2.*")]
-[assembly: AssemblyFileVersion("0.4.2.0")]
+[assembly: AssemblyVersion("0.5.0.*")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
