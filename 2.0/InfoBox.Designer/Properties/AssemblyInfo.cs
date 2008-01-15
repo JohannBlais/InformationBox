df9cf7d8be386bc5c3ecaecedb77c6f763177b2e
SaumonAgile_cp <SND\SaumonAgile_cp>
2008-01-15 22:16:16 +0000

    Renamed InfoBox.Test to InfoBox.Designer

--- 2.0/InfoBox.Designer/Properties/AssemblyInfo.cs
+++ 2.0/InfoBox.Designer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InfoBox.Test")]
+[assembly: AssemblyTitle("InfoBox.Designer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SaumonAgile")]
-[assembly: AssemblyProduct("InfoBox.Test")]
+[assembly: AssemblyProduct("InformationBox.Designer")]
 [assembly: AssemblyCopyright("Copyright © SaumonAgile 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
