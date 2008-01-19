fb45cf21ec4de7c2dbd5f9d61a22b48fafece50f
SaumonAgile_cp <SND\SaumonAgile_cp>
2008-01-19 18:47:44 +0000

    Minor changes

--- 2.0/InfoBox.Designer/Properties/AssemblyInfo.cs
+++ 2.0/InfoBox.Designer/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.6.6.*")]
-[assembly: AssemblyFileVersion("0.6.6.0")]
+[assembly: AssemblyVersion("0.6.7.*")]
+[assembly: AssemblyFileVersion("0.6.7.0")]
