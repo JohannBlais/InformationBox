0390fbd3817cde04e3e9517d9079bc1d09f80943
JohannBlais_cp <SND\JohannBlais_cp>
2013-02-25 16:36:53 +0000

    Fixed DPI handling

--- 2.0/InfoBox/Form/InformationBoxForm.Designer.cs
+++ 2.0/InfoBox/Form/InformationBoxForm.Designer.cs
@@ -189,8 +189,8 @@ namespace InfoBox
             // 
             // InformationBoxForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(257, 153);
             this.Controls.Add(this.pnlForm);
             this.DoubleBuffered = true;
