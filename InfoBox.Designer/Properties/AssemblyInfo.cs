7be907ccdd2545e95f0922e8ee4775a4c74d87de
magicandre1981 <magicandre1981@outlook.com>
2020-10-12 13:56:53 +0200

    converted old csproj to SDK style, changed net40 to net46 and added netcoreapp3.1 to TargetFrameworks

--- InfoBox.Designer/Properties/AssemblyInfo.cs
+++ InfoBox.Designer/Properties/AssemblyInfo.cs
@@ -9,21 +9,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("InfoBox.Designer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johann Blais")]
-[assembly: AssemblyProduct("InformationBox Designer")]
-[assembly: AssemblyCopyright("Copyright © Johann Blais 2007-2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Neutral language
-[assembly: NeutralResourcesLanguage("en")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -34,12 +19,3 @@ using System.Resources;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ac83595b-9fe7-4e62-a373-030d8b8aab26")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- InfoBox/Properties/AssemblyInfo.cs
+++ InfoBox/Properties/AssemblyInfo.cs
@@ -10,21 +10,6 @@ using System.Runtime.InteropServices;
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("InfoBox")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johann Blais")]
-[assembly: AssemblyProduct("InformationBox")]
-[assembly: AssemblyCopyright("Copyright © Johann Blais 2007-2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Neutral language
-[assembly: NeutralResourcesLanguage("en")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -35,15 +20,3 @@ using System.Runtime.CompilerServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e14a6ac1-494d-481d-8510-d652082e43ba")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
